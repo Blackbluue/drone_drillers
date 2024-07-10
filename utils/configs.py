6a93664acf4f52c1edaffd0e27da817eff46d70e
Raymone Miller <miller_raymone@hotmail.com>
2024-07-10 12:03:44 -0400

    Add documentation

--- utils/configs.py
+++ utils/configs.py
@@ -1,3 +1,5 @@
+"""A class to manage configuration settings for the game."""
+
 from __future__ import annotations
 
 import json
@@ -10,24 +12,36 @@ if TYPE_CHECKING:
 
 
 class Configs:
+    """A class to manage configuration settings for the game."""
+
     _configs: MutableMapping[str, Any] = {}
     _path: str = ""
 
     @classmethod
     def set_path(cls, path: str) -> None:
+        """Set the path to the configuration file.
+
+        Args:
+            path (str): The path to the configuration file.
+
+        Raises:
+            FileNotFoundError: If the file does not exist.
+        """
         if not os.path.isfile(path):
             raise FileNotFoundError(f"File not found: {path}")
         cls._path = path
 
     @classmethod
     def load(cls) -> None:
-        with open(cls._path, "r") as f:
+        """Load the configuration settings from the file."""
+        with open(cls._path, "r", encoding="utf-8") as f:
             cls._configs.clear()
             cls._configs.update(json.load(f))
 
     @classmethod
     def save(cls) -> None:
-        with open(cls._path, "w") as f:
+        """Save the configuration settings to the file."""
+        with open(cls._path, "w", encoding="utf-8") as f:
             json.dump(cls._configs, f, indent=4)
 
     def __getitem__(self, key: str) -> Any:
